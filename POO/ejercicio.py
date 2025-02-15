32a686b3dccae604a2169e380fc1fd485e22e460
Luis Viveros <luis.viveros.escamilla@continental-corporation.com>
2025-02-15 16:27:05 -0600

    add excersise POO

--- POO/ejercicio.py
+++ POO/ejercicio.py
@@ -7,5 +7,48 @@ class Empleado:
         self.nombre = nombre
         self.sueldo_base = sueldo_base
     
-    def sueldo_actual(self):
-        print("")
+    def sueldo_actual(self,dias_trabajados):
+        sueldo_actual = self.sueldo / 30 * dias_trabajados
+        return sueldo_actual
+
+    def mostrar_datos(self):
+        print(f"Nombre: {self.nombre} Salario Base: {self.sueldo_base}")
+        print("----------------------------------------------------")
+
+class Gerente(Empleado):
+    def __init__(self, nombre, sueldo_base,bono):
+        super().__init__(nombre, sueldo_base)
+        self.bono = bono
+
+    def salario_total(self):
+        total_salario =  self.sueldo_base + self.bono
+        return total_salario
+
+
+    def mostrar_datos(self):
+        print(f"Gerente - Nombre: {self.nombre}, Salario Base:{self.sueldo_base}, Bono: {self.bono} Salario Total: {self.salario_total()} ") 
+        print("----------------------------------------------------")
+
+
+class Progrmador(Empleado):
+    def __init__(self, nombre, sueldo_base,lenguaje):
+        super().__init__(nombre, sueldo_base)
+        self.lenguaje = lenguaje
+
+    def mostrar_datos(self):
+       print(f"Gerente - Nombre: ${self.nombre}, Salario Base: ${self.sueldo_base}, Lenguaje: {self.lenguaje} Salario Total: ${self.sueldo_base} ") 
+       print("----------------------------------------------------")
+        
+
+    
+
+Nombre_1 = Empleado("Juan Perez", 50000)
+Nombre_1.mostrar_datos()
+
+Nombre_2 = Gerente("Ana Gomez", 70000, 10000)
+Nombre_2.mostrar_datos()
+
+Nombre_3 = Progrmador("Carlos Rodriguez", 60000, "Python")
+Nombre_3.mostrar_datos()
+        
+
