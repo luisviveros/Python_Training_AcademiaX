3d634187a002a8c1e6707d3d625e5cfb8a39cb95
Luis Viveros <luis.viveros.escamilla@continental-corporation.com>
2025-02-20 11:14:09 -0600

    main.py

---
+++ Ejericio_compania_FAANG/main.py
@@ -0,0 +1,7 @@
+#Tienes una lista de diferentes canciones son canciones de diferentes autores y lo que haras es crear una rokola
+# Cada que presiones play te va a regresa una canción
+# Tome una cancion de las que tiene al azar y la va a tocar
+# Hay una configuracion que le tienes que poner a la rokola es un numero y ese numero lo que dice es que no repitas la cancion que ya tocaste antes despues de cierto numero de turnos 
+# Por ejemplo si este numero es 4 y pongo play voy a escoger una cancion y la siguiente vez que toque play esa cancion no se va a repetir va a tomar hasta 4 turnos para que vuelva a tocar esa cancion
+
+print("Hola")
